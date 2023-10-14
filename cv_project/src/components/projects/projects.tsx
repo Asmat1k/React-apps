827227c38d01f677a3954e7bc9bd827c95f8ecd2
Asmat1k <dobrovti@mail.ru>
2023-10-14 12:19:01 +0300

    feat: add two links for projects

--- cv_project/src/components/projects/projects.tsx
+++ cv_project/src/components/projects/projects.tsx
@@ -1,12 +1,12 @@
 import styles from './projects.module.scss';
 
 const pr_list = [
-  {name: "CSS-Selectors", description: "Игра по изучению селекторов CSS", link: "https://css-selectors-asmat1k.netlify.app/" },
-  {name: "Minesweeper", description: "Старая добрая игра сапер", link: "https://minesweeper-asmat1k.netlify.app/" },
-  {name: "Virtual Keyboard", description: "Виртуальная клавиатура с поддержкой реальной", link: "https://asmat1k.github.io/virtual-keyboard/" },
-  {name: "Shelter", description: "Сайт приюта для животных", link: "https://asmat1k.github.io/Shelter/shelter/" },
-  {name: "Plants", description: "Сайт по продаже садовых услуг", link: "https://asmat1k.github.io/plants/plants/" },
-  {name: "Css Bayan", description: "Обычный аккордион написанный на CSS", link: "https://asmat1k.github.io/cssBayan/cssBayan/index.html" },
+  {name: "CSS-Selectors", description: "Игра по изучению селекторов CSS", deploy: "https://css-selectors-asmat1k.netlify.app/", link: "https://github.com/Asmat1k/CSS-Selectors"},
+  {name: "Minesweeper", description: "Старая добрая игра сапер", deploy: "https://minesweeper-asmat1k.netlify.app/", link: "https://github.com/Asmat1k/Minesweeper" },
+  {name: "Virtual Keyboard", description: "Виртуальная клавиатура с поддержкой реальной", deploy: "https://asmat1k.github.io/virtual-keyboard/", link: "https://github.com/Asmat1k/virtual-keyboard" },
+  {name: "Shelter", description: "Сайт приюта для животных", deploy: "https://asmat1k.github.io/Shelter/shelter/", link: "https://github.com/Asmat1k/Shelter" },
+  {name: "Plants", description: "Сайт по продаже садовых услуг", deploy: "https://asmat1k.github.io/plants/plants/", link: "https://github.com/Asmat1k/plants" },
+  {name: "Css Bayan", description: "Обычный аккордион написанный на CSS", deploy: "https://asmat1k.github.io/cssBayan/cssBayan/index.html", link: "https://github.com/Asmat1k/cssBayan" },
 ];
 
 const Projects = () => {
@@ -16,10 +16,16 @@ const Projects = () => {
       <div className={styles.cards}>
         {pr_list.map((item: Record<string, string>, id) => {
           return (
-            <a href={item.link} className={styles.card} key={id} target='_blank' rel="noreferrer">
-              <h4 className={styles.name}>{item.name}</h4>
-              <div className={styles.descr}>{item.description}</div>
-            </a>
+            <div  className={styles.card} key={id}>
+              <div className={styles.info}>
+                <h4 className={styles.name}>{item.name}</h4>
+                <div className={styles.descr}>{item.description}</div>
+              </div>
+              <div className={styles.links}>
+                <a href={item.link} target='_blank' rel="noreferrer" className={styles.link}>github</a>
+                <a href={item.deploy} target='_blank' rel="noreferrer" className={styles.link}>deploy</a>
+              </div>
+            </div>
           );
         })}
       </div>
