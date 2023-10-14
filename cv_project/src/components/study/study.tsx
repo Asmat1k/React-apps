fba91e28d17cc5c02ec2b642ce9635d87103a437
Asmat1k <dobrovti@mail.ru>
2023-10-14 12:27:56 +0300

    refactor: remove some words

--- cv_project/src/components/study/study.tsx
+++ cv_project/src/components/study/study.tsx
@@ -5,8 +5,8 @@ const Study = () => {
     <div className={styles.study}>
       <h3 className={styles.title}>Обучение: </h3>
       <div className={styles.place}>
-        <div className={styles.place_title}>БГТУ "ВОЕНМЕХ"</div>
-        <div className={styles.place_dir}>Направление: Программная инженерия (Бакалавриат)</div>
+        <div className={styles.place_title}>БГТУ "ВОЕНМЕХ" им. Д.Ф. Устинова</div>
+        <div className={styles.place_dir}>Программная инженерия (Бакалавриат)</div>
         <div className={styles.place_time}>2021 - 2025</div>
       </div>
     </div>
