341ccf9d53b7c5cc4c4ed5c516002cfc6acd4ada
Asmat1k <dobrovti@mail.ru>
2023-11-19 20:02:50 +0300

    feat: add user`s page and fix redirection to it

--- posts-app/src/app/index.tsx
+++ posts-app/src/app/index.tsx
@@ -6,6 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 import { BaseLayout } from './baseLayout';
 import NotFoundPage from '../pages/not-found';
 import Post from '../pages/post';
+import UserPage from '../pages/user-page';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route index element={<Main />} />
           <Route path="posts" element={<Posts />} />
           <Route path="posts/:id" element={<Post />} />
+          <Route path="user" element={<UserPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
---
+++ posts-app/src/pages/user-page/index.tsx
@@ -0,0 +1,5 @@
+function UserPage() {
+  return <div>This is user page and it s in work</div>;
+}
+
+export default UserPage;
