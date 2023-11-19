288532fa1eb5ee9d454a7cb2a7110e2e00448c4c
Asmat1k <dobrovti@mail.ru>
2023-11-19 19:59:28 +0300

    feat: add styles for header and redirection to user`s page

--- posts-app/src/widgets/Header/UI/Header.tsx
+++ posts-app/src/widgets/Header/UI/Header.tsx
@@ -1,4 +1,5 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { UserOutlined } from '@ant-design/icons';
 import styles from './Header.module.scss';
 
 const setActive = ({ isActive }) => (isActive ? styles.active : '');
@@ -6,12 +7,31 @@ const setActive = ({ isActive }) => (isActive ? styles.active : '');
 export function Header() {
   return (
     <header className={styles.container}>
-      <NavLink className={setActive} to="/">
-        Home
-      </NavLink>
-      <NavLink className={setActive} to="/posts">
-        Posts
-      </NavLink>
+      <Link to="/" className={styles.logo}>
+        <h1 className={styles.title}>RNDM</h1>
+        <h2 className={styles.subt}>POSTS</h2>
+      </Link>
+      <div className={styles.wrapper}>
+        <nav className={styles.navigation}>
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              <NavLink className={setActive} to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className={styles.item}>
+              <NavLink className={setActive} to="/posts">
+                Posts
+              </NavLink>
+            </li>
+            <li className={styles.item}>
+              <NavLink className={setActive} to="/user">
+                <UserOutlined />
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
