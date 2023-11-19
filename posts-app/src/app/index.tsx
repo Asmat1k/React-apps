23a7fcde00e5aa8c1656325169d24a318a028f67
Asmat1k <dobrovti@mail.ru>
2023-11-19 14:29:25 +0300

    feat: add simple router to app

--- posts-app/src/app/index.tsx
+++ posts-app/src/app/index.tsx
@@ -1,7 +1,18 @@
+import Main from '../pages/main';
+import Posts from '../pages/posts';
 import './styles/index.scss';
 
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div>PostsApp Project</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="posts" element={<Posts />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
