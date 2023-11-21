7a0015683c568b2a2e8954410a7206e2b59a39ae
Asmat1k <dobrovti@mail.ru>
2023-11-21 14:08:20 +0300

    refactor: add update main page

--- posts-app/src/pages/main/index.tsx
+++ posts-app/src/pages/main/index.tsx
@@ -1,5 +1,30 @@
+import { QRCode } from 'antd';
+import styles from './main.module.scss';
+import { Link } from 'react-router-dom';
+
 function Main() {
-  return <div>Main</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.qr}>
+        <QRCode size={310} value="https://jsonplaceholder.typicode.com/" />
+      </div>
+      <div className={styles.info}>
+        <div className={styles.head}>
+          <h2 className={styles.title}>RNDM POSTS</h2>
+          <h3 className={styles.subt}>-from &lt;Json&gt;Placeholder-</h3>
+        </div>
+        <div className={styles.text}>
+          <Link className={styles.link} to="/">
+            LOGIN
+          </Link>{' '}
+          or{' '}
+          <Link className={styles.link} to="/">
+            REGISTER
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
