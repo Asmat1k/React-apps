2a7966e3628556579206064510c6718b705fd55f
Asmat1k <dobrovti@mail.ru>
2023-11-21 13:36:28 +0300

    refactor: update not-found page

--- posts-app/src/pages/not-found/index.tsx
+++ posts-app/src/pages/not-found/index.tsx
@@ -1,5 +1,11 @@
+import { Empty } from 'antd';
+import styles from './not-found.module.scss';
+import { useParams } from 'react-router-dom';
+
 function NotFoundPage() {
-  return <div>Page not found</div>;
+  const query = useParams();
+  const descriptionText = `Sorry, we didn't found "${query['*']}" page`;
+  return <Empty className={styles.empty} description={descriptionText} />;
 }
 
 export default NotFoundPage;
